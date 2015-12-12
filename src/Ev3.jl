12b8e46b503bd4dd60d43d5e7c20182f676d7ff7
Robin Deits <robin.deits@gmail.com>
2015-12-12 18:37:01 -0500

    extract some code to new libraries

--- src/Ev3.jl
+++ src/Ev3.jl
@@ -1,16 +1,20 @@
 module Ev3
 
+using Behaviors
+
 import Base: parse, read, write
 
-export Device, Sensor, Motor, run_forever, stop
+export Device,
+       Sensor, 
+       Motor
 
 SYS_ROOT = "/sys"
 
 include("nodes.jl")
 include("devices.jl")
 include("channels.jl")
-include("Behaviors.jl")
-include("mapping_behaviors.jl")
-include("Mapping.jl")
+# include("Behaviors.jl")
+# include("mapping_behaviors.jl")
+# include("Mapping.jl")
 
 end
