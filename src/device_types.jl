84e24c6c093a94c2456623b3348345bc56d0a2bf
Robin Deits <robin.deits@gmail.com>
2016-06-19 23:24:17 -0400

    more sensors

--- src/device_types.jl
+++ src/device_types.jl
@@ -1,5 +1,7 @@
 typealias LargeMotor Device{TachoMotor, Driver{:lego_ev3_l_motor}}
 typealias MediumMotor Device{TachoMotor, Driver{:lego_ev3_m_motor}}
 
-typealias Ultrasound Device{LegoSensor, Driver{:lego_ev3_us}}
+typealias UltrasoundSensor Device{LegoSensor, Driver{:lego_ev3_us}}
 typealias ColorSensor Device{LegoSensor, Driver{:lego_ev3_color}}
+typealias TouchSensor Device{LegoSensor, Driver{:lego_ev3_touch}}
+typealias GyroSensor Device{LegoSensor, Driver{:lego_ev3_gyro}}
